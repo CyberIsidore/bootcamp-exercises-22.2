8f8c400b5cd47a2b23bb1113db474014bb778a79
CyberIsidore <wizardisidore@gmail.com>
2023-06-04 15:04:18 -0400

    In-Progress (6/4)

--- python-ds-practice/36_find_the_duplicate/find_the_duplicate.py
+++ python-ds-practice/36_find_the_duplicate/find_the_duplicate.py
@@ -1,15 +1,27 @@
+nums = [1, 2, 1, 4, 3, 12]
 def find_the_duplicate(nums):
-    """Find duplicate number in nums.
+    dupes = []
+    freq = nums.count(nums)
+    for num in nums:
+        if freq >= 1:
+            dupes.append(num)
+            return dupes
+        else:
+            return "None"
 
-    Given a list of nums with, at most, one duplicate, return the duplicate.
-    If there is no duplicate, return None
 
-        >>> find_the_duplicate([1, 2, 1, 4, 3, 12])
-        1
-
-        >>> find_the_duplicate([6, 1, 9, 5, 3, 4, 9])
-        9
-
-        >>> find_the_duplicate([2, 1, 3, 4]) is None
-        True
-    """
+print(find_the_duplicate(nums))
+    # """Find duplicate number in nums.
+    #
+    # Given a list of nums with, at most, one duplicate, return the duplicate.
+    # If there is no duplicate, return None
+    #
+    #     >>> find_the_duplicate([1, 2, 1, 4, 3, 12])
+    #     1
+    #
+    #     >>> find_the_duplicate([6, 1, 9, 5, 3, 4, 9])
+    #     9
+    #
+    #     >>> find_the_duplicate([2, 1, 3, 4]) is None
+    #     True
+    # """
