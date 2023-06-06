771a1e77d065f197f45796a32140104e8aed7ef0
CyberIsidore <wizardisidore@gmail.com>
2023-06-06 10:10:08 -0400

    Complete! (6/6)

--- python-ds-practice/12_multiply_even_numbers/multiply_even_numbers.py
+++ python-ds-practice/12_multiply_even_numbers/multiply_even_numbers.py
@@ -1,30 +1,11 @@
+# Complete!
+numbers = [1, 2, 3, 4, 5, 6]
 def multiply_even_numbers(numbers):
-    evens = []
-    for num in numbers:
-        if num % 2 == 0:
-            evens.append(num)
-        return evens
-    if evens.index(0) is True:
-        for x in evens:
-            i = 1
-            product = i * x
-        return product
-    else:
-        return "....wait, you didn't give me any even numbers..."
+    evens = [num for num in numbers if num % 2 == 0]
+    result = 1
+    for x in evens:
+        result = result * x
+    return result
 
 
-    # """Multiply the even numbers.
-    #
-    #     >>> multiply_even_numbers([2, 3, 4, 5, 6])
-    #     48
-    #
-    #     >>> multiply_even_numbers([3, 4, 5])
-    #     4
-    #
-    # If there are no even numbers, return 1.
-    #
-    #     >>> multiply_even_numbers([1, 3, 5])
-    #     1
-    # """
-numbers = input("Numbers, please!: ").replace(" ", "")
-print(f"Mmmmm delicious. The product is {multiply_even_numbers(numbers)}")
+print(multiply_even_numbers(numbers))
