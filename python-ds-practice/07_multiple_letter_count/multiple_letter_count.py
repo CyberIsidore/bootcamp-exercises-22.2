b2bef0ca901bddf1ee63902359354c623e2cb415
CyberIsidore <wizardisidore@gmail.com>
2023-06-09 11:14:41 -0400

    Complete! (6/9)

--- python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
+++ python-ds-practice/07_multiple_letter_count/multiple_letter_count.py
@@ -1,28 +1,27 @@
-# get phrase from user
-# turn phrase into a list of characters (dictionary = "characters")
-# count frequency (variable = freq) of each letter (keyword = "ltr")
+# Complete!
 #
 # Count frequency of letter and add to dictionary
 # return output of letter
 # """Return dict of {ltr: frequency} from phrase.
-    #
-    #     >>> multiple_letter_count('yay')
-    #     {'y': 2, 'a': 1}
-    #
-    #     >>> multiple_letter_count('Yay')
-    #     {'Y': 1, 'a': 1, 'y': 1}
-    # """
+#
+#     >>> multiple_letter_count('yay')
+#     {'y': 2, 'a': 1}
+#
+#     >>> multiple_letter_count('Yay')
+#     {'Y': 1, 'a': 1, 'y': 1}
+# """
 def multiple_letter_count(phrase):
-    count = dict([char, x])
-    for char in phrase:
-        x = phrase.count(char)
+    chars = list(phrase)
+    freq = []
+    for char in chars:
+        freq.append(chars.count(char))
+    letter_count = dict(zip(chars, freq))
+    return letter_count
 
-    count.get('char', 'x')
-    return count
 
 
 phrase = input("""
 I can count how often letters are used in a phrase. 
 Give me a phrase, then I'll handle the rest.
->""").replace(" ", "")
+>""").replace(" ", "").casefold()
 print(multiple_letter_count(phrase))
