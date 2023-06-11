1d304ba3f0c2e5455b00b18de9f9722db395daf7
CyberIsidore <wizardisidore@gmail.com>
2023-06-11 01:04:21 -0400

    Stuck! (6/11)

--- python-ds-practice/16_partition/partition.py
+++ python-ds-practice/16_partition/partition.py
@@ -1,21 +1,28 @@
-def partition(lst, fn):
-    """Partition lst by predicate.
-     
-     - lst: list of items
-     - fn: function that returns True or False
-     
-     Returns new list: [a, b], where `a` are items that passed fn test,
-     and `b` are items that failed fn test.
+def is_even(lst):
+    return lst % 2 == 0
 
-        >>> def is_even(num):
-        ...     return num % 2 == 0
-        
-        >>> def is_string(el):
-        ...     return isinstance(el, str)
-        
-        >>> partition([1, 2, 3, 4], is_even)
-        [[2, 4], [1, 3]]
-        
-        >>> partition(["hi", None, 6, "bye"], is_string)
-        [['hi', 'bye'], [None, 6]]
-    """
+def is_string(el):
+    return isinstance(el, str)
+
+
+def partition(lst, is_even):
+    win = []
+    lose = []
+    return [win.append(x) if is_even(x) is True else lose.append(x) for x in lst]
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+print(partition(lst, is_even(lst)))
+    # """Partition lst by predicate.
+    #
+    #  - lst: list of items
+    #  - fn: function that returns True or False
+    #
+    #  Returns new list: [a, b], where `a` are items that passed fn test,
+    #  and `b` are items that failed fn test.
+    #
+    #     >>> partition([1, 2, 3, 4], is_even)
+    #     [[2, 4], [1, 3]]
+    #
+    #     >>> partition(["hi", None, 6, "bye"], is_string)
+    #     [['hi', 'bye'], [None, 6]]
+    # """
--- python-ds-practice/18_calculate/calculate.py
+++ python-ds-practice/18_calculate/calculate.py
@@ -1,28 +1,50 @@
-def calculate(operation, a, b, make_int=False, message='The result is'):
-    """Perform operation on a + b, ()possibly truncating) & returning w/msg.
+def calculate(operation, a, b, message='The result is'):
+    if operation == "a":
+        x = ""
+        return f"{message}: {x}"
+    elif operation == "s":
+        x = a - b
+        return f"{message}: {x}"
+    elif operation == "m":
+        x = a * b
+        return f"{message}: {x}"
+    elif operation == "d":
+        x = a % b
+        return f"{message}: {x}"
+    else:
+        return "That's not a valid operator."
 
-    - operation: 'add', 'subtract', 'multiply', or 'divide'
-    - a and b: values to operate on
-    - make_int: (optional, defaults to False) if True, truncates to integer
-    - message: (optional) message to use (if not provided, use 'The result is')
+operation = "a"
+a = 2.4
+b = 4
+message = "Result"
 
-    Performs math operation (truncating if make_int), then returns as
-    "[message] [result]"
 
-        >>> calculate('add', 2.5, 4)
-        'The result is 6.5'
-
-        >>> calculate('subtract', 4, 1.5, make_int=True)
-        'The result is 2'
-
-        >>> calculate('multiply', 1.5, 2)
-        'The result is 3.0'
-
-        >>> calculate('divide', 10, 4, message='I got')
-        'I got 2.5'
-
-    If a valid operation isn't provided, return None.
-
-        >>> calculate('foo', 2, 3)
-        
-    """
+print(calculate(operation, a, b, message))
+    # """Perform operation on a + b, ()possibly truncating) & returning w/msg.
+    #
+    # - operation: 'add', 'subtract', 'multiply', or 'divide'
+    # - a and b: values to operate on
+    # - make_int: (optional, defaults to False) if True, truncates to integer
+    # - message: (optional) message to use (if not provided, use 'The result is')
+    #
+    # Performs math operation (truncating if make_int), then returns as
+    # "[message] [result]"
+    #
+    #     >>> calculate('add', 2.5, 4)
+    #     'The result is 6.5'
+    #
+    #     >>> calculate('subtract', 4, 1.5, make_int=True)
+    #     'The result is 2'
+    #
+    #     >>> calculate('multiply', 1.5, 2)
+    #     'The result is 3.0'
+    #
+    #     >>> calculate('divide', 10, 4, message='I got')
+    #     'I got 2.5'
+    #
+    # If a valid operation isn't provided, return None.
+    #
+    #     >>> calculate('foo', 2, 3)
+    #
+    # """
