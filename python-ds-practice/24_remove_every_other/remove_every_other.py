dc504566f0064eb46e56db778c0a50e7b6cd8afc
CyberIsidore <wizardisidore@gmail.com>
2023-06-11 02:04:00 -0400

    Complete! (6/11)

--- python-ds-practice/24_remove_every_other/remove_every_other.py
+++ python-ds-practice/24_remove_every_other/remove_every_other.py
@@ -1,13 +1,8 @@
+# Complete!
 def remove_every_other(lst):
-    """Return a new list of other item.
+    new_lst = [x for x in lst if lst.index(x) % 2 == 0]
+    return new_lst
 
-        >>> lst = [1, 2, 3, 4, 5]
 
-        >>> remove_every_other(lst)
-        [1, 3, 5]
-
-    This should return a list, not mutate the original:
-
-        >>> lst
-        [1, 2, 3, 4, 5]
-    """
+lst = [1, 2, 3, 4, 5]
+print(remove_every_other(lst))
