4e22acebf2e7cf283201d101d0a1cb63db2871e6
CyberIsidore <wizardisidore@gmail.com>
2023-06-11 00:05:51 -0400

    Complete! (6/9)

--- python-ds-practice/13_capitalize/capitalize.py
+++ python-ds-practice/13_capitalize/capitalize.py
@@ -1,9 +1,13 @@
+# Complete!
 def capitalize(phrase):
-    """Capitalize first letter of first word of phrase.
-
-        >>> capitalize('python')
-        'Python'
-
-        >>> capitalize('only first word')
-        'Only first word'
-    """
+    return phrase.capitalize()
+    # """Capitalize first letter of first word of phrase.
+    #
+    #     >>> capitalize('python')
+    #     'Python'
+    #
+    #     >>> capitalize('only first word')
+    #     'Only first word'
+    # """
+phrase = input("Phrase please! ")
+print(f'I think you meant: "{capitalize(phrase)}."')
