7d2ed5403ba0f4149c17b3fd570df3181e44c147
CyberIsidore <wizardisidore@gmail.com>
2023-06-10 23:34:33 -0400

    Complete! 6/10)

--- python-ds-practice/11_flip_case/flip_case.py
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -6,18 +6,6 @@ def flip_case(phrase, to_swap):
         return "That character isn't in your phrase!"
 
 
-    # """Flip [to_swap] case each time it appears in phrase.
-    #
-    #     >>> flip_case('Aaaahhh', 'a')
-    #     'aAAAhhh'
-    #
-    #     >>> flip_case('Aaaahhh', 'A')
-    #     'aAAAhhh'
-    #
-    #     >>> flip_case('Aaaahhh', 'h')
-    #     'AaaaHHH'
-    #
-    # """
 phrase = input("Say something! ")
 print(f'You said: "{phrase}"')
 to_swap = input("What letter should we flip? ").casefold()
